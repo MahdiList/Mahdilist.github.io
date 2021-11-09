d5f47988a815fd6091ae772e5b50b668d8bd601c
0marcc <77485298+0marcc@users.noreply.github.com>
2021-11-09 03:33:39 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //v3.4 Add popup describing app when visitors load webpage the first time
 window.onload = function() {
-    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+    alert("Welcome to 'Shopping List' App!\n\nCreated Using Javascript\nby Rock Valley College student Omar Mahdi");
     populateshoppinglistonload();
     displayShoppinglists();
     clearFocus();
@@ -24,7 +24,7 @@ function get(name){
 //ShareList passbyvalues Week 14
 function passlist()
 {
- var url = "https://rvclist.github.io/rvclist14/index.html?list="+ shoppinglist;
+ var url = "https://Mahdilist.github.io/MahdiList/index.html?list="+ shoppinglist;
  //Week 14 add link to sharelist id
       document.getElementById("sharelist").innerHTML = 'Share List:\n' + url;
  //Copy URL
@@ -51,7 +51,7 @@ function copyToClipboard(text) {
 
 function about()
 {
-    alert("Welcome to 'Shopping List' App!\n\nCreated by Rock Valley College\n**Javascript(Web233) Students**\n\nQuestions?\nemail Professor Chuck Konkol\nc.konkol@rockvalleycollege.edu\n\nRegister @ RockValleyCollege.edu");
+    alert("Welcome to 'Shopping List' App!\n\nCreated Using Javascript\nby Rock Valley College student Omar Mahdi");
     
 }
 //read cookie and return
